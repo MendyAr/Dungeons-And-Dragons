57d8e569930ac76e6c14a671bd7b65338405ad59
Mendy Aharonian <mendya@post.bgu.ac.il>
2021-06-02 20:26:05 +0300

    position, comparators, tile

--- src/BusinessLayer/Empty.java
+++ src/BusinessLayer/Empty.java
@@ -3,8 +3,7 @@ package BusinessLayer;
 public class Empty extends Tile{
 
     //constructors
-    public Empty(Integer px, Integer py){
-        super(px, py);
-        tileChar = '.';
+    public Empty(){
+        super('.');
     }
 }
