8aaccab821ec3ce6492a44b84474a5d5502ad6d4
Mendy Aharonian <mendya@post.bgu.ac.il>
2021-06-11 16:15:17 +0300

    .

---
+++ src/test/java/BusinessLayer/Tiles/Classes/WarriorTest.java
@@ -0,0 +1,26 @@
+package BusinessLayer.Tiles.Classes;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WarriorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void turn() {
+        assertTrue(true);
+    }
+
+    @Test
+    void castAbility() {
+    }
+
+    @Test
+    void lvlUp() {
+    }
+}
